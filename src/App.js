c18e2a634ecbb1c937010094629ce7317f597988
rizkygusna <gusnafarid@gmail.com>
2021-11-26 11:17:46 +0700

    feat: sorting based on upvotes & recent

--- src/App.js
+++ src/App.js
@@ -2,24 +2,46 @@ import React from 'react';
 import './App.css';
 import 'h8k-components';
 
+import { useState } from 'react';
+
 import Articles from './components/Articles';
 
-const title = "Sorting Articles";
+const title = 'Sorting Articles';
 
-function App({articles}) {
+function App({ articles }) {
+  const [data, setData] = useState(articles);
 
-    return (
-        <div className="App">
-            <h8k-navbar header={title}></h8k-navbar>
-            <div className="layout-row align-items-center justify-content-center my-20 navigation">
-                <label className="form-hint mb-0 text-uppercase font-weight-light">Sort By</label>
-                <button data-testid="most-upvoted-link" className="small">Most Upvoted</button>
-                <button data-testid="most-recent-link" className="small">Most Recent</button>
-            </div>
-            <Articles articles={articles}/>
-        </div>
-    );
+  const handleUpvote = () => {
+    //sort data without mutating the prev data
+    const sortedData = [...data].sort((a, b) => b.upvotes - a.upvotes);
+    setData(sortedData);
+  };
 
+  const handleRecent = () => {
+    const sortedData = [...data].sort((a, b) => {
+      const dateA = new Date(a.date),
+        dateB = new Date(b.date);
+      //sort in descending order
+      return dateB - dateA;
+    });
+    setData(sortedData);
+  };
+
+  return (
+    <div className='App'>
+      <h8k-navbar header={title}></h8k-navbar>
+      <div className='layout-row align-items-center justify-content-center my-20 navigation'>
+        <label className='form-hint mb-0 text-uppercase font-weight-light'>Sort By</label>
+        <button data-testid='most-upvoted-link' className='small' onClick={handleUpvote}>
+          Most Upvoted
+        </button>
+        <button data-testid='most-recent-link' className='small' onClick={handleRecent}>
+          Most Recent
+        </button>
+      </div>
+      <Articles articles={data} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,28 +1,27 @@
 import React from 'react';
 
-function Articles() {
-
-    return (
-        <div className="card w-50 mx-auto">
-            <table>
-                <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Upvotes</th>
-                    <th>Date</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr data-testid="article" key="article-index">
-                    <td data-testid="article-title">Article 1 title</td>
-                    <td data-testid="article-upvotes">Article 1 title</td>
-                    <td data-testid="article-date">Article 1 title</td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
+function Articles({ articles }) {
+  return (
+    <div className='card w-50 mx-auto'>
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Upvotes</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        {articles.map((article) => (
+          <tr data-testid='article' key={article.date}>
+            <td data-testid='article-title'>{article.title}</td>
+            <td data-testid='article-upvotes'>{article.upvotes}</td>
+            <td data-testid='article-date'>{article.date}</td>
+          </tr>
+        ))}
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Articles;
